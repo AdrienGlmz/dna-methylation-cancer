8df53df8a72dd4962dfd18aae76ea54eeadb07f3
Adrien Galamez <adrien.galamez@slalom.com>
2020-04-21 10:37:24 -0400

    Update dataproc processing files to account for one row per sample

--- dataproc_processing/create_cpg_sites_list.py
+++ dataproc_processing/create_cpg_sites_list.py
@@ -3,7 +3,7 @@
 
 # Import modules
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split, monotonically_increasing_id, concat, lit, when
+from pyspark.sql.functions import split, monotonically_increasing_id, concat, lit, when, substring
 from pyspark.sql.types import DoubleType, StringType
 
 spark = (SparkSession
@@ -17,10 +17,19 @@ df = (spark
       .read
       .format("csv")
       .option("header", "true")
-      .load("gs://build_hackathon_dnanyc/raw_data_clustered/betas_patients_partition_*.csv"))
-df = df.withColumn('label', when(df.BRCA_ParticipantCode == "non-BRCA", 0).otherwise(1))
+      .load("gs://build_hackathon_dnanyc/raw_data_clustered_v2/betas_patients_partition_*.csv")
+      .drop('sample_id').drop('BRCA_ParticipantCode').drop('row_number'))
 
+print("Reading from gs://build_hackathon_dnanyc/raw_data_clustered_v2/betas_patients_partition_*.csv")
+
+# Rename column sample_status into label
+df = df.withColumnRenamed('sample_status', 'label')
+
+# Cast beta values as float
 df = df.withColumn("beta_value",df.beta_value.cast(DoubleType()))
+
+# Create the sample id column
+df = df.withColumn('sample_id', substring('aliquot_barcode', 1, 16))
 
 df.printSchema()
 
@@ -28,16 +37,16 @@ df.printSchema()
 df_count = df.groupby('CpG_probe_id').count()
 df_count = df_count.withColumnRenamed('count', 'count_cpg')
 
-patient_count = df.select('participant_id').dropDuplicates().count()
+sample_count = df.select('sample_id').dropDuplicates().count()
 
 # Variance by group
-mean_by_cpg = df.groupby('CpG_probe_id').pivot('label', ['0', '1']).avg('beta_value')
-mean_by_cpg = mean_by_cpg.withColumnRenamed('1', 'cancer').withColumnRenamed('0', 'normal')
+mean_by_cpg = df.groupby('CpG_probe_id').pivot('label', ['tumor', 'normal']).avg('beta_value')
+mean_by_cpg = mean_by_cpg.withColumnRenamed('tumor', 'cancer').withColumnRenamed('normal', 'normal')
 
 cpg_df = mean_by_cpg.join(df_count, ['CpG_probe_id'], 'left')
 cpg_df.printSchema()
 
-cpg_df = cpg_df.withColumn('freq', cpg_df.count_cpg / patient_count)
+cpg_df = cpg_df.withColumn('freq', cpg_df.count_cpg / sample_count)
 cpg_df = cpg_df.filter(cpg_df.freq > 0.9)
 cpg_df = cpg_df.drop('count_cpg').drop('freq')
 
@@ -55,9 +64,12 @@ mean_by_cpg = mean_by_cpg.withColumn("sum_squared_between_groups",
                                       + (mean_by_cpg.cancer - mean_by_cpg.avg_beta)**2))
 mean_by_cpg = mean_by_cpg.orderBy('sum_squared_between_groups', ascending=False)
 
-top_cpg = mean_by_cpg.select('CpG_probe_id').limit(5000)
+n_cols = 5000
+top_cpg = mean_by_cpg.select('CpG_probe_id').limit(n_cols)
 top_cpg = top_cpg.rdd.map(lambda x: x.CpG_probe_id)
+print("Outputting {} columns".format(n_cols))
 
 
 # Save to GCS
-top_cpg.repartition(12).saveAsTextFile('gs://build_hackathon_dnanyc/columns_to_keep')
+print("Saving into gs://build_hackathon_dnanyc/columns_to_keep_v2")
+top_cpg.repartition(12).saveAsTextFile('gs://build_hackathon_dnanyc/columns_to_keep_v2')
--- dataproc_processing/pivot_table_clustered.py
+++ dataproc_processing/pivot_table_clustered.py
@@ -3,7 +3,7 @@
 
 # Import modules
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split, monotonically_increasing_id, concat, lit, when
+from pyspark.sql.functions import split, monotonically_increasing_id, concat, lit, when, substring
 from pyspark.sql.types import DoubleType, StringType
 
 spark = (SparkSession
@@ -22,33 +22,40 @@ for i in range(1, 11001, 100):
           .read
           .format("csv")
           .option("header", "true")
-          .load("gs://build_hackathon_dnanyc/raw_data_clustered/betas_patients_partition_{}_*.csv".format(i)))
-    df = df.withColumn('label', when(df.BRCA_ParticipantCode == "non-BRCA", 0).otherwise(1))
+          .load("gs://build_hackathon_dnanyc/raw_data_clustered_v2/betas_patients_partition_{}_*.csv".format(i))
+          .drop('sample_id').drop('BRCA_ParticipantCode').drop('row_number'))
+
+    print("Reading from gs://build_hackathon_dnanyc/raw_data_clustered_v2/betas_patients_partition_{}_*.csv".format(i))
+    # df = df.withColumn('label', when(df.BRCA_ParticipantCode == "non-BRCA", 0).otherwise(1))
+    df = df.withColumnRenamed('sample_status', 'label')
 
     df = df.withColumn("beta_value",df.beta_value.cast(DoubleType()))
 
+    df = df.withColumn('sample_id', substring('aliquot_barcode', 1, 16))
+
     df.printSchema()
 
-    # Variance by group
-    top_cpg = spark.sparkContext.textFile('gs://build_hackathon_dnanyc/columns_to_keep/')
+    # Get cpg_site list
+    top_cpg = spark.sparkContext.textFile('gs://build_hackathon_dnanyc/columns_to_keep_v2/')
+    print("Reading from gs://build_hackathon_dnanyc/columns_to_keep_v2/")
     top_cpg = top_cpg.collect()
 
 
     # Pivot table
     pivot_df = (df
-          .select(['beta_value', 'CpG_probe_id', 'participant_id'])
-          .groupBy('participant_id')
+          .select(['beta_value', 'CpG_probe_id', 'sample_id'])
+          .groupBy('sample_id')
           .pivot('CpG_probe_id', top_cpg)
           .avg('beta_value')
          )
     pivot_df = (pivot_df
-                .join(df.select('participant_id', 'label').dropDuplicates(), ['participant_id'], 'left')
+                .join(df.select('sample_id', 'label').dropDuplicates(), ['sample_id'], 'left')
                )
 
     # Load data
     print("Data Engineering done!")
-    print("Loading into GCS....")
+    print("Loading to gs://build_hackathon_dnanyc/processed_data_v2/pivot_partition_{}.csv....".format(i))
     (pivot_df
     .coalesce(32)
-    .write.csv('gs://build_hackathon_dnanyc/processed_data/pivot_partition_{}.csv'.format(i), header='true')
+    .write.csv('gs://build_hackathon_dnanyc/processed_data_v2/pivot_partition_{}.csv'.format(i), header='true')
     )
