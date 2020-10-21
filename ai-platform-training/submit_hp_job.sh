532fa65127a612ae557a6a5165d8cb668d94eb0f
Adrien Galamez <adrien.galamez@slalom.com>
2020-10-21 16:35:51 -0400

    Change script to linux script

--- ai-platform-training/submit_hp_job.sh
+++ ai-platform-training/submit_hp_job.sh
@@ -1,18 +1,10 @@
-#!/bin/bash
-BUCKET_NAME="dna-methylation-cancer"
-JOB_NAME="dna_methylation_xgboost_binary_$(date +"%Y%m%d_%H%M%S")"
-IMAGE_URI="gcr.io/gcp-nyc/dna-methylation-cancer:xgboost-binary"
-JOB_DIR="gs://$BUCKET_NAME/job_dir"
-HPTUNING_CONFIG="./trainer/hptuning_config_xgboost.yaml"
-REGION="us-east1"
-SCALE_TIER=CUSTOM
-MASTER_MACHINE_TYPE="n1-standard-4"
-
-gcloud ai-platform jobs submit training $JOB_NAME \
-  --region $REGION \
-  --master-image-uri $IMAGE_URI \
-  --scale-tier $SCALE_TIER \
-  --master-machine-type $MASTER_MACHINE_TYPE \
-  --config $HPTUNING_CONFIG
-  -- \
-  --model-dir=$JOB_DIR \
+export BUCKET_NAME="dna-methylation-cancer"
+export JOB_NAME="dna_methylation_xgboost_binary_$(date +"%Y%m%d_%H%M%S")"
+export IMAGE_URI="gcr.io/gcp-nyc/dna-methylation-cancer:xgboost-binary"
+export MODEL_DIR="gs://$BUCKET_NAME/job_dir/$(date +%Y%m%d_%H%M%S)"
+export HPTUNING_CONFIG="trainer/hptuning_config_xgboost.yaml"
+export REGION="us-east1"
+export SCALE_TIER="custom"
+export MASTER_MACHINE_TYPE="n1-standard-4"
+echo $HPTUNING_CONFIG
+gcloud ai-platform jobs submit training $JOB_NAME --region $REGION  --master-image-uri $IMAGE_URI --config $HPTUNING_CONFIG -- --model-dir=$MODEL_DIR
