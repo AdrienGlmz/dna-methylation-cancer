117f27a06a60b0de3396041f7eaf76e6ca728820
Adrien Galamez <adrien.galamez@slalom.com>
2020-10-22 10:44:56 -0400

    Clean and document notebook

--- ai-platform-training/submit_hp_job.sh
+++ ai-platform-training/submit_hp_job.sh
@@ -1,10 +1,10 @@
-export BUCKET_NAME="dna-methylation-cancer"
-export JOB_NAME="dna_methylation_xgboost_binary_$(date +"%Y%m%d_%H%M%S")"
-export IMAGE_URI="gcr.io/gcp-nyc/dna-methylation-cancer:xgboost-binary"
-export MODEL_DIR="gs://$BUCKET_NAME/job_dir/$(date +%Y%m%d_%H%M%S)"
-export HPTUNING_CONFIG="trainer/hptuning_config_xgboost.yaml"
-export REGION="us-east1"
-export SCALE_TIER="custom"
-export MASTER_MACHINE_TYPE="n1-standard-4"
+export BUCKET_NAME=dna-methylation-cancer
+export JOB_NAME=dna_methylation_xgboost_binary_$(date +"%Y%m%d_%H%M%S")
+export IMAGE_URI=gcr.io/gcp-nyc/dna-methylation-cancer:xgboost-binary
+export MODEL_DIR=gs://$BUCKET_NAME/job_dir/$(date +%Y%m%d_%H%M%S)
+export HPTUNING_CONFIG=trainer/hptuning_config_xgboost.yaml
+export REGION=us-east1
+export SCALE_TIER=custom
+export MASTER_MACHINE_TYPE=n1-standard-4
 echo $HPTUNING_CONFIG
 gcloud ai-platform jobs submit training $JOB_NAME --region $REGION  --master-image-uri $IMAGE_URI --config $HPTUNING_CONFIG -- --model-dir=$MODEL_DIR
--- configs/configs.py
+++ configs/configs.py
@@ -2,13 +2,30 @@
 PROJECT_ID = 'gcp-nyc'
 DATASET = 'dna_cancer_prediction'
 GCS_BUCKET = 'dna-methylation-cancer'
+REGION = "us-east1"
 
 # Preprocessing variables
 RAW_DATASET_PATH = 'training_data/tcga-binary.csv'
+RAW_LABEL_NAME = 'sample_status'
+RAW_INDEX_NAME = 'aliquot_barcode'
+
+# Training variables
+TRAINING_LABEL_NAME = 'labels'
 TRAIN_DATASET_PATH = 'training_data/train.csv'
 TEST_DATASET_PATH = 'training_data/test.csv'
-LABEL_NAME = 'sample_status'
-INDEX_NAME = 'aliquot_barcode'
 
-# Trainig variables
-TRAINING_LABEL_NAME = 'labels'
+# AI Platform training job
+IMAGE_REPO_NAME = 'gcr.io/gcp-nyc/dna-methylation-cancer'
+IMAGE_TAG = 'xgboost-binary'
+IMAGE_URI = f"gcr.io/{PROJECT_ID}/{IMAGE_REPO_NAME}:{IMAGE_TAG}"
+TRAINING_DOCKERFILE = 'ai-platform-training/Dockerfile'
+HPTUNING_CONFIG = 'ai-platform-training/trainer/hptuning_config_xgboost.yaml'
+
+# AI Platform prediction
+MODEL_NAME = 'dna_cancer_prediction'
+VERSION_NAME = 'xgboost_binary_test'
+ORIGIN = 'gs://dna-methylation-cancer/job_dir/20201021_204518/'
+PACKAGE_URIS = 'gs://dna-methylation-cancer/prediction_routine/my_custom_code-0.2.tar.gz'
+PREDICTION_CLASS = 'predictor.MyPredictor'
+RUNTIME_VERSION = '2.2'
+PYTHON_VERSION = '3.7'
